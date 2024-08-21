61e2e239ea0a145bee4752d4a039de7ec3068406
Felipe <felipesorrentino22102003@gmail.com>
2024-08-21 10:18:54 -0300

    Projeto, pedra, papel e tesoura.
    Desenvolvendo um projeto na linguagem de javaScript.

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 /* Desenvolva seu código abaixo */
-console.log("Olá felipito05!")
+function playRockPaperScissor(player1,player2){
+
+player1.toUpperCase(1);
+player2.toUpperCase(1);  
+const pedra = "Pedra";
+const papel = "Papel";
+const tesoura = "Tesoura";
+
+if(player1 == pedra && player2 == tesoura){
+    return "Jogador 1 venceu!"
+}
+else if(player1 == pedra && player2== papel){
+    return "Jogador 2 venceu!"
+}
+else if(player1 == pedra && player2 == pedra){
+    return "Empate!"
+}
+else if(player1 == papel && player2 == pedra){
+    return "Jogador 1 venceu!"
+}
+else if(player1 == papel && player2 == tesoura){
+    return "Jogador 2 venceu!"
+}
+else if(player1 == papel && player2 == papel){
+    return "Empate!"
+}
+else if(player1 == tesoura && player2 == papel){
+    return "Jogador 1 venceu!"
+}
+else if(player1 == tesoura && player2 == pedra){
+    return "Jogador 2 venceu!"
+}
+else if(player1 == tesoura && player2 == tesoura){
+    return "Empate!"
+}
+}
+console.log(playRockPaperScissor("papel", "tesoura"));
